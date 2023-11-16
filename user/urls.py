af55119f3af2799c50ca80cd01e283bd59309237
Davydova Olha <o.chookity@gmail.com>
2023-11-16 17:33:28 +0200

    Add manage user page URL to urlpatterns

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
 
 
 urlpatterns = [
@@ -13,6 +13,7 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
