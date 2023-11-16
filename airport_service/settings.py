5a380ede5b5fb1d0cc0adc5368f86b7d5d2f1b57
Davydova Olha <o.chookity@gmail.com>
2023-11-16 19:57:49 +0200

    Add drf_spectacular settings

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_spectacular",
     "airport",
     "user"
 ]
@@ -131,6 +132,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
