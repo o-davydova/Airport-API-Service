a1547f5d9ce6a2c4af849c9b2e622235ec2205d6
Davydova Olha <o.chookity@gmail.com>
2023-11-16 17:45:18 +0200

    Add router to airport urls & register `AirplaneTypeViewSet`

--- airport/urls.py
+++ airport/urls.py
@@ -1,3 +1,12 @@
-urlpatterns = []
+from rest_framework import routers
+
+from airport.views import AirplaneTypeViewSet
+
+
+router = routers.DefaultRouter()
+
+router.register("airplane_types", AirplaneTypeViewSet)
+
+urlpatterns = router.urls
 
 app_name = "airport"
